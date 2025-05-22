7f3485644146b75804cf72fc9e5439155e70f475
Milad Afzali <miladsteam455@gmail.com>
2025-05-22 22:04:34 +0330

    Update Logic Game

--- module/winLoseGame.js
+++ module/winLoseGame.js
@@ -3,6 +3,9 @@ import * as updateScore from "./updateScore.js";
 export default function (choises) {
   const resultsGameWraper = document.querySelector(".results-game");
   const btnResultsGame = document.querySelector(".results-game__btn");
+  const titleResultGame = document.querySelector(
+    ".results-game__title"
+  ).firstElementChild;
   const pickedWraper = document.querySelector(".start-game");
   const btnGameWraper = document.querySelector(".body-app__btn-wraper");
 
@@ -19,13 +22,23 @@ export default function (choises) {
   ).lastElementChild;
 
   resultsGameWraper.classList.remove("hidden");
+  console.log(choises[0]);
+  console.log(choises[1]);
   if (choises[0] === choises[1]) {
+    titleResultGame.textContent = "game even";
+  } else if (
+    (choises[0] === "scissors-btn" && choises[1] === "paper-btn") ||
+    (choises[0] === "paper-btn" && choises[1] === "rock-btn") ||
+    (choises[0] === "rock-btn" && choises[1] === "scissors-btn")
+  ) {
     btnResultsGame.firstElementChild.classList.add("dark");
     playerPicked.classList.add("win");
+    titleResultGame.textContent = "you win";
     updateScore.addScore();
   } else {
     btnResultsGame.firstElementChild.classList.add("red");
     botPicked.classList.add("win");
+    titleResultGame.textContent = "you lose";
     updateScore.reduceScore();
   }
 }
