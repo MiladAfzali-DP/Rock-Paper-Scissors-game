825ab853f9d2cbb51dafe2c1569880d991a2b4cf
Milad Afzali <miladsteam455@gmail.com>
2025-09-01 02:57:47 +0330

    complated Rule Modale

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,18 +3,23 @@ import "./App.css";
 import RpcGame from "../RpcGame/RpcGame";
 import Button from "../Button/Button";
 import PopUp from "../PopUp/PopUp";
+import Modale from "../Modale/Modale";
 
 export default function App() {
   const [score, setScore] = useState(+localStorage.getItem("score") || 0);
   const [isOpenPopUp, setIsOpenPopUp] = useState(false);
+  const [isOpenModale, setIsOpenModale] = useState(false);
 
+  // Handle Func
   const handleOpenPopUp = () => setIsOpenPopUp(true);
   const handleClosePopUp = () => setIsOpenPopUp(false);
+
+  const handleOpenModale = () => setIsOpenModale(true);
+  const handleCloseModale = () => setIsOpenModale(false);
   return (
     <section className="app">
       {isOpenPopUp && (
         <PopUp onClosePopUp={handleClosePopUp}>
-          {" "}
           <p>Your Score: {score}</p>
         </PopUp>
       )}
@@ -28,7 +33,21 @@ export default function App() {
         Save Score
       </Button>
       <RpcGame score={score} setScore={setScore} />
-      <Button className="btn btn-rule">rules</Button>
+      {isOpenModale && (
+        <Modale
+          onClick={() => {
+            handleCloseModale();
+          }}
+        />
+      )}
+      <Button
+        className="btn btn-rule"
+        onClick={() => {
+          handleOpenModale();
+        }}
+      >
+        rules
+      </Button>
     </section>
   );
 }
